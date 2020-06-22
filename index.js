00a31f64c6aa9e59a3b1a421a6f3496dabd478e4
Nicolas Bovatick <fariasnico16@gmail.com>
2020-06-22 14:37:07 -0300

    modificaciones para Heroku

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const port = process.env.PORT || 3000;
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello world');
 });
 
-app.listen(3000);
+app.listen(port);
